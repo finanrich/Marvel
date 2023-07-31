495945cf646aa865d62b5ef25d90c468173a7713
Nazar <nazarfreelance11@gmail.com>
2023-07-31 21:37:29 +0300

    transform object character

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/app/App';
 import './style/style.scss';
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  	<React.StrictMode>
+    	<App />
+  	</React.StrictMode>,
+  	document.getElementById('root')
 );
 
