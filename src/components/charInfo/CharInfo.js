57d1149185b33c4f787376e708fdfed5c65439b2
Nazar <nazarfreelance11@gmail.com>
2023-08-04 16:54:44 +0300

    adding focus and keyboard control

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
@@ -124,6 +125,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import MarvelService from '../services/MarvelServices';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import Spinner from '../spinner/Spinner';
 
 import './charList.scss';
 
-class CharList extends Component{
+class CharList extends Component {
 
     state = {
         charList: [],
@@ -15,8 +17,7 @@ class CharList extends Component{
         offset: 210,
         charEnded: false
     }
-
-
+    
     marvelService = new MarvelService();
 
     componentDidMount() {
@@ -33,7 +34,7 @@ class CharList extends Component{
     onCharListLoading = () => {
         this.setState({
             newItemLoading: true
-        });
+        })
     }
 
     onCharListLoaded = (newCharList) => {
@@ -41,7 +42,6 @@ class CharList extends Component{
         if (newCharList.length < 9) {
             ended = true;
         }
-
 
         this.setState(({offset, charList}) => ({
             charList: [...charList, ...newCharList],
@@ -54,60 +54,94 @@ class CharList extends Component{
 
     onError = () => {
         this.setState({
-            loading: false,
-            error: true
+            error: true,
+            loading: false
         })
     }
 
-    renderItems(arr) {
-        const items = arr.map((item) => {
-            let imgObjFit = {'objectFit' : 'cover'};
-            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-                imgObjFit = {'objectFit' : 'unset'}
-            }
+    itemRefs = [];
 
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        // Я реализовал вариант чуть сложнее, и с классом и с фокусом
+        // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+        // На самом деле, решение с css-классом можно сделать, вынеся персонажа
+        // в отдельный компонент. Но кода будет больше, появится новое состояние
+        // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+    renderItems(arr) {
+        const items =  arr.map((item, i) => {
+            let imgStyle = {'objectFit' : 'cover'};
+            if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+                imgStyle = {'objectFit' : 'unset'};
+            }
+            
             return (
-                <li className="char__item"
-                key={item.id}
-                onClick={() => this.props.onCharSelected(item.id)}>
-                    <img src={item.thumbnail} alt={item.name} style={imgObjFit}/>
-                    <div className="char__name">{item.name}</div>
+                <li 
+                    className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
+                    key={item.id}
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
+                        <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+                        <div className="char__name">{item.name}</div>
                 </li>
             )
         });
+        // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
             </ul>
         )
-    }    
+    }
 
-   render() {
-    const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+    render() {
 
-    const items = this.renderItems(charList);
-    const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error) ? items : null;
+        const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
+        
+        const items = this.renderItems(charList);
 
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? items : null;
 
         return (
             <div className="char__list">
-                {items}
                 {errorMessage}
                 {spinner}
                 {content}
-                <button
+                <button 
                     className="button button__main button__long"
                     disabled={newItemLoading}
-                    onClick={() => this.onRequest(offset)}
-                    style={{'display' : charEnded ? 'none' : 'block'}}>
-                    
+                    style={{'display': charEnded ? 'none' : 'block'}}
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
         )
-   }
+    }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
 }
 
 export default CharList;
